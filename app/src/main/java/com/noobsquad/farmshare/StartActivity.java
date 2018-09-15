d2c30ceb2ad4d005b44e97062b2543b569f52c74
jatin-jt <sachinjatingupta@gmail.com>
2018-09-15 16:12:27 +0530

    Added the maps activity and setup google maps sdk

--- app/src/main/java/com/noobsquad/farmshare/StartActivity.java
+++ app/src/main/java/com/noobsquad/farmshare/StartActivity.java
@@ -1,13 +1,19 @@
 package com.noobsquad.farmshare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
 
+    Button btnMarkLand;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        btnMarkLand = findViewById(R.id.btn_mark_land);
+        btnMarkLand.setOnClickListener((View v)->{startActivity(new Intent(this,MapsActivity.class));});
     }
 }
